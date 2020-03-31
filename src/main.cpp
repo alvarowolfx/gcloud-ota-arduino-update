fdf837ee2f9075911feea1f188f9d6b4aecb3a53
Lorenzo Romagnoli <lorenzormgnl@gmail.com>
2020-03-31 13:43:29 +0200

    new tag

--- src/main.cpp
+++ src/main.cpp
@@ -28,203 +28,203 @@ ESP8266WebServer server(80);
 WebServer server(80);
 #endif
 
-/* 
+/*
  * Check if needs to update the device and returns the download url.
  */
 String getDownloadUrl()
 {
-  HTTPClient http;
-  String downloadUrl;
-  USE_SERIAL.print("[HTTP] begin...\n");
+	HTTPClient http;
+	String downloadUrl;
+	USE_SERIAL.print("[HTTP] begin...\n");
 
-  String url = CLOUD_FUNCTION_URL;
-  url += String("?version=") + CURRENT_VERSION;
-  url += String("&variant=") + VARIANT;
-  http.begin(url);
+	String url = CLOUD_FUNCTION_URL;
+	url += String("?version=") + CURRENT_VERSION;
+	url += String("&variant=") + VARIANT;
+	http.begin(url);
 
-  USE_SERIAL.print("[HTTP] GET...\n");
-  // start connection and send HTTP header
-  int httpCode = http.GET();
+	USE_SERIAL.print("[HTTP] GET...\n");
+	// start connection and send HTTP header
+	int httpCode = http.GET();
 
-  // httpCode will be negative on error
-  if (httpCode > 0)
-  {
-    // HTTP header has been send and Server response header has been handled
-    USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+	// httpCode will be negative on error
+	if (httpCode > 0)
+	{
+		// HTTP header has been send and Server response header has been handled
+		USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-    // file found at server
-    if (httpCode == HTTP_CODE_OK)
-    {
-      String payload = http.getString();
-      USE_SERIAL.println(payload);
-      downloadUrl = payload;
-    } else {
-      USE_SERIAL.println("Device is up to date!");
-    }
-  }
-  else
-  {
-    USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
-  }
+		// file found at server
+		if (httpCode == HTTP_CODE_OK)
+		{
+			String payload = http.getString();
+			USE_SERIAL.println(payload);
+			downloadUrl = payload;
+		} else {
+			USE_SERIAL.println("Device is up to date!");
+		}
+	}
+	else
+	{
+		USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+	}
 
-  http.end();
+	http.end();
 
-  return downloadUrl;
+	return downloadUrl;
 }
 
-/* 
+/*
  * Download binary image and use Update library to update the device.
  */
 bool downloadUpdate(String url)
 {
-  HTTPClient http;
-  USE_SERIAL.print("[HTTP] Download begin...\n");
+	HTTPClient http;
+	USE_SERIAL.print("[HTTP] Download begin...\n");
 
-  http.begin(url);
+	http.begin(url);
 
-  USE_SERIAL.print("[HTTP] GET...\n");
-  // start connection and send HTTP header
-  int httpCode = http.GET();
-  if (httpCode > 0)
-  {
-    // HTTP header has been send and Server response header has been handled
-    USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+	USE_SERIAL.print("[HTTP] GET...\n");
+	// start connection and send HTTP header
+	int httpCode = http.GET();
+	if (httpCode > 0)
+	{
+		// HTTP header has been send and Server response header has been handled
+		USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
 
-    // file found at server
-    if (httpCode == HTTP_CODE_OK)
-    {
+		// file found at server
+		if (httpCode == HTTP_CODE_OK)
+		{
 
-      int contentLength = http.getSize();
-      USE_SERIAL.println("contentLength : " + String(contentLength));
+			int contentLength = http.getSize();
+			USE_SERIAL.println("contentLength : " + String(contentLength));
 
-      if (contentLength > 0)
-      {
-        bool canBegin = Update.begin(contentLength);
-        if (canBegin)
-        {
-          WiFiClient stream = http.getStream();
-          USE_SERIAL.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
-          size_t written = Update.writeStream(stream);
+			if (contentLength > 0)
+			{
+				bool canBegin = Update.begin(contentLength);
+				if (canBegin)
+				{
+					WiFiClient stream = http.getStream();
+					USE_SERIAL.println("Begin OTA. This may take 2 - 5 mins to complete. Things might be quite for a while.. Patience!");
+					size_t written = Update.writeStream(stream);
 
-          if (written == contentLength)
-          {
-            USE_SERIAL.println("Written : " + String(written) + " successfully");
-          }
-          else
-          {
-            USE_SERIAL.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
-          }
+					if (written == contentLength)
+					{
+						USE_SERIAL.println("Written : " + String(written) + " successfully");
+					}
+					else
+					{
+						USE_SERIAL.println("Written only : " + String(written) + "/" + String(contentLength) + ". Retry?");
+					}
 
-          if (Update.end())
-          {
-            USE_SERIAL.println("OTA done!");
-            if (Update.isFinished())
-            {
-              USE_SERIAL.println("Update successfully completed. Rebooting.");
-              ESP.restart();
-              return true;
-            }
-            else
-            {
-              USE_SERIAL.println("Update not finished? Something went wrong!");
-              return false;
-            }
-          }
-          else
-          {
-            USE_SERIAL.println("Error Occurred. Error #: " + String(Update.getError()));
-            return false;
-          }
-        }
-        else
-        {
-          USE_SERIAL.println("Not enough space to begin OTA");
-          client.flush();
-          return false;
-        }
-      }
-      else
-      {
-        USE_SERIAL.println("There was no content in the response");
-        client.flush();
-        return false;
-      }
-    }
-    else
-    {
-      return false;
-    }
-  }
-  else
-  {
-    return false;
-  }
+					if (Update.end())
+					{
+						USE_SERIAL.println("OTA done!");
+						if (Update.isFinished())
+						{
+							USE_SERIAL.println("Update successfully completed. Rebooting.");
+							ESP.restart();
+							return true;
+						}
+						else
+						{
+							USE_SERIAL.println("Update not finished? Something went wrong!");
+							return false;
+						}
+					}
+					else
+					{
+						USE_SERIAL.println("Error Occurred. Error #: " + String(Update.getError()));
+						return false;
+					}
+				}
+				else
+				{
+					USE_SERIAL.println("Not enough space to begin OTA");
+					client.flush();
+					return false;
+				}
+			}
+			else
+			{
+				USE_SERIAL.println("There was no content in the response");
+				client.flush();
+				return false;
+			}
+		}
+		else
+		{
+			return false;
+		}
+	}
+	else
+	{
+		return false;
+	}
 }
 
-/* 
+/*
  * Show current device version
  */
-void handleRoot() {  
-  server.send(200, "text/plain", "v" + String(CURRENT_VERSION));
+void handleRoot() {
+	server.send(200, "text/plain", "v" + String(CURRENT_VERSION));
 }
 
 void setup()
 {
-  Serial.begin(115200);
-  Serial.setDebugOutput(true);  
-  pinMode(LED_BUILTIN, OUTPUT);
+	Serial.begin(115200);
+	Serial.setDebugOutput(true);
+	pinMode(LED_BUILTIN, OUTPUT);
 
-  delay(3000);
-  Serial.println("\n Starting");
-  // Setup Wifi Manager
-  String version = String("<p>Current Version - v") + String(CURRENT_VERSION) + String("</p>");
-  USE_SERIAL.println(version);
-  
-  WiFiManager wm;
-  WiFiManagerParameter versionText(version.c_str());
-  wm.addParameter(&versionText);    
-    
-  if (!wm.autoConnect()) {
-    Serial.println("failed to connect and hit timeout");
-    //reset and try again, or maybe put it to deep sleep
-    ESP.restart();
-    delay(1000);
-  }
- 
-  // Check if we need to download a new version
-  String downloadUrl = getDownloadUrl();
-  if (downloadUrl.length() > 0)
-  {
-    bool success = downloadUpdate(downloadUrl);
-    if (!success)
-    {
-      USE_SERIAL.println("Error updating device");
-    }
-  }
+	delay(3000);
+	Serial.println("\n Starting");
+	// Setup Wifi Manager
+	String version = String("<p>Current Version - v") + String(CURRENT_VERSION) + String("</p>");
+	USE_SERIAL.println(version);
 
-  server.on("/", handleRoot);
-  server.begin();
-  USE_SERIAL.println("HTTP server started");
+	WiFiManager wm;
+	WiFiManagerParameter versionText(version.c_str());
+	wm.addParameter(&versionText);
 
-  USE_SERIAL.print("IP address: ");
-  USE_SERIAL.println(WiFi.localIP());
+	if (!wm.autoConnect()) {
+		Serial.println("failed to connect and hit timeout");
+		//reset and try again, or maybe put it to deep sleep
+		ESP.restart();
+		delay(1000);
+	}
+
+	// Check if we need to download a new version
+	String downloadUrl = getDownloadUrl();
+	if (downloadUrl.length() > 0)
+	{
+		bool success = downloadUpdate(downloadUrl);
+		if (!success)
+		{
+			USE_SERIAL.println("Error updating device");
+		}
+	}
+
+	server.on("/", handleRoot);
+	server.begin();
+	USE_SERIAL.println("HTTP server started");
+
+	USE_SERIAL.print("IP address: ");
+	USE_SERIAL.println(WiFi.localIP());
 }
 
 int ledState = LOW;
-const long interval = 1000;
+const long interval = 500;
 unsigned long previousMillis = 0;
 
 void loop()
 {
 
-  unsigned long currentMillis = millis();
+	unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= interval) {    
-    previousMillis = currentMillis;
-    ledState = ledState == LOW ? HIGH : LOW;
-    digitalWrite( BUILTIN_LED, ledState );
-  }
-  
-  // Just chill
-  server.handleClient();
+	if (currentMillis - previousMillis >= interval) {
+		previousMillis = currentMillis;
+		ledState = ledState == LOW ? HIGH : LOW;
+		digitalWrite( BUILTIN_LED, ledState );
+	}
+
+	// Just chill
+	server.handleClient();
 }
